eca307dd8eb1e46364a55b462b0e305439eb3de6
wint <6386129+Wintus@users.noreply.github.com>
2025-03-17 21:02:39 +0900

    fix setup

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
 	test: {
 		globals: true,
 		environment: 'happy-dom',
-		setupFiles: ['./src/test-setup.ts'],
 		include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
 	},
 });
