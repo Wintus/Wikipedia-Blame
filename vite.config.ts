3f3d3d00da5f840c5f8cd68ee85dbfac2454b2a2
wint <6386129+Wintus@users.noreply.github.com>
2025-03-17 20:59:06 +0900

    configure Vite for testing with DOM testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	test: {
+		globals: true,
+		environment: 'happy-dom',
+		setupFiles: ['./src/test-setup.ts'],
+		include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+	},
+});
