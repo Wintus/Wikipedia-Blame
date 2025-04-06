d80b4eb4a9c59d000809b56c0c2e8be2c4011bb9
wint <6386129+Wintus@users.noreply.github.com>
2025-04-06 11:31:00 +0900

    fmt

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,11 @@ import {
 	fetchPageId,
 } from './services/WikipediaAPI';
 import { findOneOccurrence } from './utils/RevisionFinder';
-import { defaultSearchResult, type SearchResult, type OnSearchFn } from './wiki';
+import {
+	defaultSearchResult,
+	type SearchResult,
+	type OnSearchFn,
+} from './wiki';
 
 function App() {
 	const [searchResult, setSearchResult] =
