9698bc0cf86a36b2851a01d9e8f8aaa09d58dab0
wint <6386129+Wintus@users.noreply.github.com>
2025-04-12 14:35:26 +0900

    refactor: exract itemGenerator from findOneOccurrence for better sampling and yielding

--- src/utils/item-finder.ts
+++ src/utils/item-finder.ts
@@ -4,54 +4,62 @@ export type Predicate<S, T extends NonNullish> = (item: S) => T | null;
 
 const batchSize = 50;
 
-/**
- * Finds an occurrence of a target string in a set of items.
- * Starts with a randomized sampling approach, then falls back to a full batch search exhaustively if necessary.
- */
 export async function findOneOccurrence<T extends number, U extends NonNullish>(
 	predicate: Predicate<U, T>,
 	fetcher: (items: ReadonlyArray<T>) => Promise<ReadonlyArray<U>>,
 	items: AsyncGenerator<T, unknown, unknown>
 ): Promise<T | null> {
 	const samplingRatio = 0.1;
-	const sampledItems = [];
 	const sampledCount = 50;
-	const fallbackItems = [];
 	const fallbackCount = 200;
+	// fetch and find in batches
+	for await (const batch of itemGenerator(
+		items,
+		samplingRatio,
+		sampledCount,
+		fallbackCount
+	)) {
+		const found = await fetchAndFind(predicate, fetcher, batch);
+		if (found != null) return found;
+	}
+	// if no item is found, return null
+	return null;
+}
+
+/**
+ * Finds an occurrence of a target string in a set of items.
+ * Starts with a randomized sampling approach, then falls back to a full batch search exhaustively if necessary.
+ */
+async function* itemGenerator<T extends number>(
+	items: AsyncGenerator<T, unknown, unknown>,
+	samplingRatio: number,
+	sampledCount: number,
+	fallbackCount: number
+): AsyncGenerator<ReadonlyArray<T>, void, unknown> {
+	const sampledItems: T[] = [];
+	const fallbackItems: T[] = [];
+	// main loop for sampling and yielding in different frequencies
 	for await (const item of items) {
-		// Randomized sampling
 		if (Math.random() < samplingRatio) {
 			sampledItems.push(item);
 		} else {
 			fallbackItems.push(item);
 		}
-		// Fetch and check for target condition over sampled items
-		if (sampledItems.length > sampledCount) {
-			const found = await fetchAndFind(
-				predicate,
-				fetcher,
-				sampledItems.splice(0, batchSize)
-			);
-			if (found != null) return found;
+		// yield a batch of items if the sampled or fallback items reach each threshold
+		if (sampledItems.length >= sampledCount) {
+			yield sampledItems.splice(0, batchSize) as ReadonlyArray<T>;
 		}
-		// Fetch and check for target condition over fallback items
-		if (fallbackItems.length > fallbackCount) {
-			const found = await fetchAndFind(
-				predicate,
-				fetcher,
-				fallbackItems.splice(0, batchSize)
-			);
-			if (found != null) return found;
+		if (fallbackItems.length >= fallbackCount) {
+			yield fallbackItems.splice(0, batchSize) as ReadonlyArray<T>;
 		}
 	}
-	// consume the rest of the items
-	const rest = [...sampledItems, ...fallbackItems];
-	if (rest.length > 0) {
-		const found = await batchSearch(predicate, fetcher, rest);
-		if (found != null) return found;
+	// yield remaining items
+	if (sampledItems.length > 0) {
+		yield* batches(batchSize, sampledItems as ReadonlyArray<T>);
 	}
-	// if no item is found, return null
-	return null;
+	if (fallbackItems.length > 0) {
+		yield* batches(batchSize, fallbackItems as ReadonlyArray<T>);
+	}
 }
 
 function* batches<T>(
