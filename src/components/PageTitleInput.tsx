33d1b263fb4c0230007a60d538b053fd6d3cc685
wint <6386129+Wintus@users.noreply.github.com>
2025-10-19 12:09:15 +0900

    fix(lint): rewrite without async keyword
    ESLint blames the async callback.

--- src/components/PageTitleInput.tsx
+++ src/components/PageTitleInput.tsx
@@ -15,19 +15,18 @@ export function PageTitleInput({
 	const [pageTitle, setPageTitle] = useState(initialPageTitle);
 	const debouncedPageTitle = useDebounce(pageTitle.trim(), 300);
 
-	const pageIdPromise = useMemo(async () => {
+	const pageIdPromise = useMemo(() => {
 		// guard
 		if (!debouncedPageTitle) {
-			return {};
+			return Promise.resolve({});
 		}
 		// fetch page ID
-		try {
-			const id = await fetchPageId(wikiUrl, debouncedPageTitle);
-			return { id: id.toString() };
-		} catch (error) {
-			console.error('Error fetching page ID:', error);
-			return { error: 'Error fetching page ID. Please try again.' };
-		}
+		return fetchPageId(wikiUrl, debouncedPageTitle)
+			.then((id) => ({ id: id.toString() }))
+			.catch((error) => {
+				console.error('Error fetching page ID:', error);
+				return { error: 'Error fetching page ID. Please try again.' };
+			});
 	}, [wikiUrl, debouncedPageTitle]);
 
 	return (
